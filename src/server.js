1e09d87e7134bdcf255d35160096748e82fcdec0
Cesar Gil Lao <cesargillao@hotmail.com>
2023-01-23 10:13:00 -0400

    Formateo y guardado de información obtenida

--- src/server.js
+++ src/server.js
@@ -1,35 +1,49 @@
+const fs = require('fs');
+const path = require('path');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const express = require('express');
 const app = express()
 const PORT = 3333;
 
+const pages = [1,2,3,4,5,6,7,8];
 
-const url = `https://www.carlogos.org/car-brands/`
+const brands = [];
 
-/* al navegar a esta pagina con axios nos retorna una promesa*/
-axios(url)
-  .then(response => {
-    const html = response.data
-    const $ = cheerio.load(html)
-    const cardContent = []
+const search = (page) => {
+  const url = `https://www.carlogos.org/car-brands/${page > 1 ? `page-${page}.html` : ''}`;
+  /* al navegar a esta pagina con axios nos retorna una promesa*/
+  return axios(url);
+}
 
+const format = (response) => {
+  const html = response.data
+  const $ = cheerio.load(html)
+  const cardContent = []
 
-    $('.logo-list li', html).each(function() {
-      const brand = $(this).find('a img').attr('alt')
-      const urlLogo = $(this).find('a img').attr('src');
-      const urlClean = `https://www.carlogos.org${urlLogo}`
+  $('.logo-list li', html).each(function() {
+    const brand = $(this).find('a img').attr('alt').replace(' logo', '');
+    const urlLogo = $(this).find('a img').attr('src');
+    const urlClean = `https://www.carlogos.org${urlLogo}`
 
-      cardContent.push({
-        brand,
-        logo: urlClean
-      })
-
-    })
-
-    console.log(cardContent)
-  
+    brands.push({
+      brand,
+      logo: urlClean
+    });
   })
+
+  // console.log(cardContent)
+
+}
+
+Promise.all(pages.map(search))
+  .then(res => {
+    res.map(format);
+    // console.log(brands);
+    const loc = path.join(__dirname, '../../brands.json');
+    fs.writeFileSync(loc, JSON.stringify(brands, null, 2));
+    process.exit();
+  }).catch(console.log);
 
 
 /*
